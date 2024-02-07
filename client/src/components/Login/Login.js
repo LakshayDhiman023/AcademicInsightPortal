cb7c05527cf03a3caa564256231a5a991b2e89fb
AYUSH KUMAR <ayushkumar@AYUSHs-Laptop.local>
2024-02-08 01:40:15 +0530

    Login page ver 2.0

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -25,12 +25,12 @@ function Login() {
   };
 
   return (
-    <div className="bg-blue-900 rounded-lg py-5">
-    <div className="container flex flex-col mx-auto bg-white rounded-lg pt-12 my-5">
-      <div className="flex justify-center w-full h-full my-auto xl:gap-14 lg:justify-normal md:gap-5 draggable">
-        <div className="flex items-center justify-center w-full lg:p-12">
-          <div className="flex items-center relative flex flex-col mt-6 text-gray-700 bg-blue shadow-md bg-clip-border rounded-xl w-96 xl:p-10">
-            <div className="flex flex-col w-full h-full pb-6 text-center bg-white rounded-3xl">
+    <div className="bg-blue-900  flex h-screen items-center justify-center">
+    <div className="container flex flex-col mx-auto bg-white rounded-lg">
+       <div className="flex items-center justify-center w-full lg:p-12  relative flex flex-col text-gray-700 bg-blue shadow-2xl bg-clip-border rounded-xl w-96 xl:p-10 ">
+          <div className="flex items-center relative flex flex-col text-gray-700 bg-blue shadow-2xl bg-clip-border rounded-xl w-96 xl:p-10"  >
+            <div className="flex flex-col w-full h-full pb-10 text-center bg-white rounded-3xl">
+              
               <div>
                 <h3 className="mb-3 text-4xl font-extrabold text-dark-grey-900">
                   {user} Login
@@ -38,17 +38,21 @@ function Login() {
                 <p className="mb-4 text-grey-700">
                   Enter your email and password
                 </p>
-                <div>
-                  <p className="mb-2 text-sm text-start text-grey-900">Email</p>
+              </div>
+
+              <div>
+                  <p className="mb-2  text-start text-grey-900">Email</p>
                   <input
                     type="email"
                     name="email"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     onChange={(e) => onValueChange(e)}
                   />
-                </div>
+              </div>
+              
+                <div className="mt-5">
                 <div>
-                  <p className="mb-2 text-sm text-start text-grey-900">
+                  <p className="mb-2  text-start text-grey-900">
                     Password
                   </p>
                   <input
@@ -58,27 +62,29 @@ function Login() {
                     onChange={(e) => onValueChange(e)}
                   />
                 </div>
+                </div>
+               
                <div className="mt-5">
                 <div>
                 <button className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={handleSubmit}>
                   Login
-                </button>
-                  
+                </button>   
                   </div>
                 </div>
+
                 <div className="mt-5">
                 <div>
-                <button className="" onClick={() => changeUser()}>
+                <button className="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={() => changeUser()}>
                   {user === "Student"
-                    ? "Login as a Admin"
-                    : "Login as a Student"}
+                    ? "Login as Admin"
+                    : "Login as Student"}
                 </button>
                 </div>
+               
                 </div>
               </div>
-            </div>
           </div>
-        </div>
+        
       </div>
     </div>
     </div>
