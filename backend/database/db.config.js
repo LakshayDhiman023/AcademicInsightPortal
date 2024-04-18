8b572671e173d432955c3a589fdd415c5f15e1d3
LakshayDhiman023 <dhimanlakshay.02@gmail.com>
2024-04-18 22:30:25 +0530

    created azure sql connection

--- backend/database/db.config.js
+++ backend/database/db.config.js
@@ -1,14 +1,16 @@
 import mysql from 'mysql'
+import fs from "fs"
 // var mysql      = require('mysql');
 
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'L@kshay.07',
-  database : 'academicinsightportal'
-});
+// var connection = mysql.createConnection({
+//   host     : 'dbms202211043.mysql.database.azure.com',
+//   user     : 'LakshayDhiman',
+//   password : 'L@kshay.07',
+//   database : 'academicinsightportal'
+// });
  
 
 
 
+var connection=mysql.createConnection({host:"dbms202211043.mysql.database.azure.com", user:"LakshayDhiman", password:"L@kshay.07", database:"academicinsightportal", port:3306, ssl: true });
 export default connection
